f8db50f0e97e7d63d5aebfb4fca15e03582fbb7a
Your Name <your.email@example.com>
2026-02-04 20:51:30 +0530

    Quick update

---
+++ variable.tf
@@ -0,0 +1,33 @@
+variable "project_name" {
+    type = string
+}
+
+variable "environment" {
+    type = string
+  
+}
+
+
+variable "sg_name" {
+    type = string            #mandatory 
+}
+
+variable "sg_description" {
+    type = string
+    default = ""          #optional
+}
+
+variable "vpc_id" {
+    type = string
+  
+}
+
+variable "sg_description" {
+    type = string
+    default = {}          #optional
+}
+
+variable "sg_tags" {
+    type = map 
+    default = {}
+}
